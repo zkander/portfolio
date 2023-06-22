cfc888ad3f00b80ed414f9666e0ea26ef376fab9
Aly-as2105591 <110607789+Aly-as2105591@users.noreply.github.com>
2023-06-22 15:56:38 +0300

    feat: add navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <a className="btn btn-ghost normal-case text-xl">Aly Soliman</a>
+    </div>
+  );
+}
