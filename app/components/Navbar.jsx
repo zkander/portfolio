932afaed82baaf5bd4f1e34d6da5802b1455d15c
Aly Soliman <alysoliman05@gmail.com>
2023-06-24 18:24:00 +0300

    remove navbar

--- app/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar() {
-  return (
-    <div className="navbar bg-neutral text-neutral-content">
-      <a className="btn btn-ghost normal-case text-xl">Aly Soliman</a>
-    </div>
-  );
-}
