47c4f91009080bd048103dc6823022ba342470af
Aly Soliman <alysoliman05@gmail.com>
2023-07-05 03:05:17 +0300

    feat: add navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar transparent">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Homepage</a>
+            </li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="navbar-center">
+        <a className=" text-[11px] sm:text-base font-serif italic">
+          "The cure for ignorance is to question. - Prophet Muhammad (PBUH)"
+        </a>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Stars from "./components/Stars";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         {children}
         <Stars />
       </body>
