e97419159147f6affc385da22366ee1ac3fb65d5
Aly Soliman <alysoliman05@gmail.com>
2023-12-04 17:38:18 +0300

    feat: add `resume pdf` to resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function page() {
-  return <div>resume</div>;
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <iframe src="/Aly_Soliman_Resume.pdf" className="w-[70%] h-[92vh]" />
+    </div>
+  );
 }
 
 export default page;
